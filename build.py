f2244f833f76a649bf56258431865ee140ab6bbc
N.Sukegawa <nsukeg@gmail.com>
2014-11-23 16:08:13 +0900

    Fix build script.
    Bugs:
    * "git checkout"  didn't work because of line ending in "git rev-parse" output.
    * protobuf build not triggered even when needed.
    * "git rev-parse" triggered before "git clone" is executed.
    
    Other:
    * Add --clean switch.
    * Rename --skip-source to --skip-setup because it is not just downloading.
    * Use fixed upstream version of protobuf.
    * Remove dead code.

--- build.py
+++ build.py
@@ -3,8 +3,10 @@
 import argparse
 import multiprocessing
 import os
-import sys
+import shutil
+import string
 import subprocess
+import sys
 
 ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
 DEPS_DIR = os.path.join(ROOT_DIR, 'deps')
@@ -32,44 +34,41 @@ class GitDependency(object):
     self.name = name
     self.url = url
     self.dir = os.path.join(DEPS_DIR, name)
-    self.ref = self._rev_parse(ref)
-
-  def execute(self, cmd, cwd=DEPS_DIR, err=None):
-    if not err:
-      err = ' '.join(cmd)
-    print(err)
-    return True
-    # return subprocess.Popen(cmd, cwd=cwd).wait() == 0
+    self.ref = ref
+    self.source_updated = False
 
   def _rev_parse(self, ref):
     p = subprocess.Popen(['git', 'rev-parse', ref], cwd=self.dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     o, _ = p.communicate()
-    return o
+    return string.strip(o)
 
   def get_source(self):
-    if os.path.exists(os.path.join(self.dir, '.git')):
-      current = self._rev_parse('HEAD')
-      if current == self.ref:
-        sys.stdout.write('Source is already up to date.\n')
-        return True
-      cmds = [
-        sys.stdout.write('Checking out %s\n' % self.ref)
-        ['git', 'fetch', 'origin'],
-        ['git', 'checkout', '-f', self.ref],
-      ]
-      return all(execute(cmd, cwd=self.dir) for cmd in cmds)
-    else:
+    should_clone = not os.path.exists(os.path.join(self.dir, '.git'))
+    if should_clone:
       cmd = [
         'git',
         'clone',
         self.url,
         self.name,
       ]
-      return execute(cmd)
+      if execute(cmd) != 0:
+        sys.stderr.write('Failed to clone source repository.\n')
+        return False
+    target = self._rev_parse(self.ref)
+    current = self._rev_parse('HEAD')
+    if current == target:
+      sys.stdout.write('Local source is up to date.\n')
+      return True
+    sys.stdout.write('Checking out %s\n' % target)
+    self.source_updated = True
+    cmds = []
+    if not should_clone:
+      cmds.append(['git', 'fetch', 'origin'])
+    cmds.append(['git', 'checkout', '-f', target])
+    return all(execute(cmd, cwd=self.dir) for cmd in cmds)
 
-  def prepare(self, skip_source=False):
-    if not skip_source:
-      self.get_source()
+  def prepare(self, skip_setup=False):
+    return skip_setup or self.get_source()
 
 
 class CppGitDependency(GitDependency):
@@ -89,26 +88,41 @@ class CppGitDependency(GitDependency):
     ]
     return all(execute(cmd, cwd=self.dir) for cmd in cmds)
 
-  def prepare(self, skip_source=False):
-    return super(CppGitDependency, self).prepare(skip_source) and self.build()
+  def prepare(self, skip_setup=False):
+    return skip_setup or super(CppGitDependency, self).prepare(False) and not self.source_updated or self.build()
 
 
 def main(argv):
   p = argparse.ArgumentParser()
+  p.add_argument('--clean', action='store_true', help='Cleanup deps directory.')
   p.add_argument('--skip-tests', action='store_true', help='Do not trigger tests.')
-  p.add_argument('--skip-source', action='store_true', help='Do not trigger dependency download.')
+  p.add_argument('--skip-setup', action='store_true', help='Do not trigger dependency setup and use files previously setup.')
   p.add_argument('--skip-deps', action='store_true', help='Do not use downloaded dependencies.')
   p.add_argument('--gyp-includes', "-I", help='Use additional gyp include.', nargs='+')
   args = p.parse_args(argv)
 
   deps = [
     GitDependency('gyp', 'http://git.chromium.org/external/gyp.git', 'origin/master'),
-    CppGitDependency('protobuf', 'http://github.com/google/protobuf.git', 'origin/master'),
+    CppGitDependency('protobuf', 'http://github.com/google/protobuf.git', 'v2.6.1'),
   ]
+
+  if args.clean:
+    clean_dirs = [
+      'bin',
+      'lib',
+      'include',
+    ]
+    clean_dirs.extend([dep.name for dep in deps])
+    for dir in clean_dirs:
+      p = os.path.join(DEPS_DIR, dir)
+      if os.path.exists(p):
+        sys.stdout.write('Cleaning up "%s".\n' % p)
+        shutil.rmtree(p)
+    return 0
 
   if not args.skip_deps:
     for dep in deps:
-      dep.prepare(args.skip_source)
+      dep.prepare(args.skip_setup)
 
   gyp_cmd = [
     'gyp' if args.skip_deps else os.path.join(DEPS_DIR, 'gyp', 'gyp'),
@@ -153,8 +167,7 @@ def main(argv):
 
   env = os.environ
   env['LD_LIBRARY_PATH'] = os.path.join(DEPS_DIR, 'lib')
-  res = all(execute(cmd, cwd=ROOT_DIR, env=env) for cmd in cmds)
-  return 0 if res else 1
+  return 0 if all(execute(cmd, cwd=ROOT_DIR, env=env) for cmd in cmds) else 1
 
 
 if __name__ == '__main__':
