def1e068e9f3052e15bef246ae8071c02bc4c7a0
Nobuaki Sukegawa <nsukeg@gmail.com>
2015-05-10 00:48:49 +0900

    Uninline a method

--- lib/protobuf/qml/processor.h
+++ lib/protobuf/qml/processor.h
@@ -37,20 +37,7 @@ signals:
   }
 
   // TODO: need timeout
-  Q_INVOKABLE void write(int tag, const QVariant& data) {
-    max_tag_ = std::max(tag, max_tag_);
-    if (!desc_) {
-      qWarning("Descriptor is null");
-      error(tag, "Descriptor is null.");
-      return;
-    }
-    auto msg = desc_->dataToMessage(data);
-    if (!msg) {
-      error(tag, "Failed to convert message data to protobuf message object.");
-      return;
-    }
-    doWrite(tag, *msg);
-  }
+  Q_INVOKABLE void write(int tag, const QVariant& data);
 
   Q_INVOKABLE void writeEnd(int tag) {
     max_tag_ = std::max(tag, max_tag_);
